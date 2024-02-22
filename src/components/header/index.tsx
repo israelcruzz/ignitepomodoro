cc5508176283900a270cf79173fb934a03bc270f
israelcruzz <israelcruz.contato@gmail.com>
2024-02-22 01:47:03 -0300

    feat: creating header style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,29 @@
-const Header = () => {
-    return <h1>Header</h1>
-}
+import { NavLink } from "react-router-dom";
+import logo from "../../../public/ignite.svg";
+import { Timer, Scroll } from "phosphor-react";
 
-export default Header
+const Header = () => {
+  return (
+    <header className="w-full flex justify-between items-center">
+      <img src={logo} />
+      <nav className="flex gap-2 text-[#E1E1E6]">
+        <NavLink
+          to="/"
+          className="w-[3rem] h-[3rem] flex items-center justify-center border-b-2 border-transparent hover:border-b-2 hover:border-b-[#00875F]"
+          title="Timer"
+        >
+          <Timer size={24} />
+        </NavLink>
+        <NavLink
+          to="/history"
+          className="w-[3rem] h-[3rem] flex items-center justify-center border-b-2 border-transparent hover:border-b-2 hover:border-b-[#00875F]"
+          title="Historico"
+        >
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -3,10 +3,13 @@ import Header from "../components/header";
 
 const DefaultLayout = () => {
   return (
-    <>
+    <div
+      className="max-w-[74rem] h-[80vh] - 10rem  bg-[#202024] p-[2.5rem] rounded-lg flex flex-col"
+      style={{ margin: "4rem auto" }}
+    >
       <Header />
       <Outlet />
-    </>
+    </div>
   );
 };
 
